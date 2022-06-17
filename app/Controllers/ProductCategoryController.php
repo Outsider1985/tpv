dc3babf53f827470611a4e80e3db111d414f5714
smartwhale139 <javieru2001@gmail.com>
2022-06-17 20:10:39 +0200

    push 17/06

--- app/Controllers/ProductCategoryController.php
+++ app/Controllers/ProductCategoryController.php
@@ -2,22 +2,21 @@
 
 namespace app\Controllers;
 
+require_once 'app/Models/ProductCategory.php';
+
+use app\Models\ProductCategory;
+
 class ProductCategoryController {
 
-	public function index(){
-        $categorias = [
-            "1" => [
-                "nombre" => "Tapas",
-            ],
-            "2" => [
-                "nombre" => "Refrescos",
-            ],
-            "3" => [
-                "nombre" => "Bebidas calientes",
-            ]
-        ];
+	protected $category;
 
-        return $categorias;
+	public function __construct(){  
+
+		$this->category = new ProductCategory();
+	}
+
+	public function index(){
+		return $this->category->index();
 	}
 }
 
--- app/Controllers/TableController.php
+++ app/Controllers/TableController.php
@@ -2,61 +2,22 @@
 
 namespace app\Controllers;
 
-// require_once 'app/Models/Table.php';
+require_once 'app/Models/Table.php';
 
-// use app\Models\Table;
+use app\Models\Table;
 
 class TableController {
 
-    // protected $table;
+	protected $table;
 
-    // public function __construct() {
-    //     $this->table = new Table();
-    // }
+	public function __construct(){  
 
-	public function index(){
-        $mesas = [
-            "1" => [
-                "numero" => "1",
-                "estado" => "0"
-            ],
-            "2" => [
-                "numero" => "2",
-                "estado" => "1"
-            ],
-            "3" => [
-                "numero" => "3",
-                "estado" => "1"
-            ],
-            "4" => [
-                "numero" => "4",
-                "estado" => "1"
-            ],
-            "5" => [
-                "numero" => "5",
-                "estado" => "0"
-            ],
-            "6" => [
-                "numero" => "6",
-                "estado" => "1"
-            ],
-            "7" => [
-                "numero" => "7",
-                "estado" => "0"
-            ],
-            "8" => [
-                "numero" => "8",
-                "estado" => "1"
-            ],
-            "9" => [
-                "numero" => "9",
-                "estado" => "0"
-            ]
-        ];
-
-        return $mesas;
+		$this->table = new Table();
 	}
 
+	public function index(){
+		return $this->table->index();
+	}
 }
 
 ?>
